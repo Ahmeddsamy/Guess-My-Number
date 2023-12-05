a206ba8432dc249d974e0710b65a7f78e18f680f
Ahmed Samy <engahmedeltonamly@gmail.com>
2023-12-05 22:05:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 'use strict';
-
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = ' Correct Number';
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 20;
-
-// document.querySelector('.guess').value = 23;
-// document.querySelector('.guess').value;
-
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
